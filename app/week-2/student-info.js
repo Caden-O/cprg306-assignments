afff8748c543d7b5e362fb55792d782d7b3ecb3c
Caden-O <caden.ortt@edu.sait.ca>
2025-09-17 10:10:43 -0600

    second commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+function StudentInfo(){
+  return(
+    <main>
+      <h1>Student Info</h1>
+      <p>Name: Caden</p>
+      <p>Repo: [Repo Link]</p>
+    </main>
+  )
+}
+
+export default StudentInfo;
