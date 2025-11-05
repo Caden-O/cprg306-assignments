503add90786c3d6eced20d1c477efccaf91c1100
Caden-O <caden.ortt@edu.sait.ca>
2025-11-04 22:28:18 -0700

    working on fixing vercel build bug

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,16 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SEND_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
+// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// let analytics;
+// if(typeof window !== undefined) {
+//   analytics = getAnalytics(app);
+// }
+// const auth = getAuth(app);
+// export { app, auth, analytics }
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
