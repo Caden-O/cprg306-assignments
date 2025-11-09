1727a6c521e6ae4cdfe0f8ad2b4a9f3a64c5043a
Caden-O <caden.ortt@edu.sait.ca>
2025-11-08 17:18:50 -0700

    week 5 feedback updates

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -3,8 +3,6 @@ import Item from "./item.js";
 import itemsJson from "./items.json"
 import { useState } from "react";
 
-
-
 function ItemList(){
   const [sortBy, setSortBy] = useState("name");
 
@@ -16,23 +14,15 @@ function ItemList(){
   )
 
   function updateSort(event){
-
     const newSortValue = event.target.value;
     setSortBy(newSortValue);
     let newItemsList = [...itemsJson];
 
-    // if(newSortValue === "name"){
-    //   newItemsList.sort((a, b) => (a.name - b.name));
-    // }else {
-    //   newItemsList.sort((a, b) => (a.category - b.category));
-    // }
     if(newSortValue=="name"){
       newItemsList.sort((a, b) => (a.name.localeCompare(b.name)));
     }else{
       newItemsList.sort((a, b) => (a.category.localeCompare(b.category)));
-      //this part is still a little confusing to me ):
     }
-
     setItems([...newItemsList]);
     // console.log(`sort by: ${event.target.value}/${newSortValue}`);
   }
@@ -40,7 +30,6 @@ function ItemList(){
   return(
     <section className="border-2 border-white bg-gray-900 rounded-2xl p-3 m-5">
       <div className="gap-2">
-
         <label htmlFor="button">Sort By: </label>
 
         <button name="button" onClick={updateSort} id="sortName" value="name"
@@ -54,17 +43,13 @@ function ItemList(){
         >Category</button>
 
       </div> 
-
       <div>
         <ul id="pageList">
 
           {/* <Item itemName="Malk" itemCount="3" itemCategory="Dairy"></Item> */}
           {items.map((item) => { return(
-              <Item onLoad={updateSort}
-                        //    ^ so when the siute starts (before buttons are pressed) the items are displayed already sorted (rather than unsorted by default ^_^)
-                        // (BECAUSE the sort by name button appears "clicked" by default when the page is loaded so it wouldnt make sense if the uhh list wasnt sorted)
+              <Item
               itemName={item.name} itemCount={item.quantity} itemCategory={item.category} key={item.id}/>
-              //this part took me ttoo long to figure out unfortunately
             )}
           )}
           
